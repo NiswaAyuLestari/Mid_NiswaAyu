1e858efaf161af2a78c8f9f6c528e695550adaa6
DESKTOP-EO39B50\LENOVO PC <niswaayu99@gmail.com>
2021-11-09 10:07:58 +0700

    Commit Ke 3

---
+++ resources/views/PengertianVSC.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pengertian VSC</h1>
+    <p> Version control adalah sebuah sistem yang mencatat setiap perubahan terhadap sebuah berkas atau kumpulan berkas sehingga pada suatu saat anda dapat kembali kepada salah satu versi dari berkas tersebut.</p>
+</body>
+</html>
